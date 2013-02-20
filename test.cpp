b75c8061792dc3dbd68752e380b88d5887cf2b14
Alessandro Sanino <saninoale@gmail.com>
2013-02-20 01:00:02 -0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,11 @@
+/**
+    20-02-2013 
+    Sanino Alessandro - Create
+**/
+
+#include "../include/Server.hpp"
+#include "../include/Client.hpp"
+
+int main(int argc, char* argv[]) {
+    return (0);
+}
